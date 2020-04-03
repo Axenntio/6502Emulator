6678e4778ff1b5ac82555bc5bf7aa7c73a7bccb5
Axenntio <axenntio@live.com>
2020-04-03 11:26:00 +0200

    Fix dump displaying

--- Device.cpp
+++ Device.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <fstream>
 #include <iterator>
+#include <algorithm>
 #include "Device.hh"
 
 Device::Device(std::string name, uint16_t bus_position) : _name(name), _bus_position(bus_position) {
@@ -41,22 +42,24 @@ void Device::writeByte(uint16_t index, uint8_t value) {
 std::ostream& operator<<(std::ostream& os, Device& device) {
 	std::cout << device.getName() << std::endl;
 	std::cout << "0x" << std::setfill('0') << std::setw(4) << std::hex << device.getOffset() << " - 0x" << device.getOffset() + device.getSize() - 1;
-	for (uint16_t i = 0; i < device.getSize(); i++) {
-		if (!(i % 16)) {
-			if (i) {
-				os << "\t";
-				for (uint16_t j = i - 16; j < i; j++) {
-					if (std::isprint(device.readByte(j + device.getOffset())))
-						os << device.readByte(j + device.getOffset());
-					else
-						os << ".";
-				}
+	for (uint16_t i = 0; i < device.getSize(); i += 16) {
+		os << std::endl << std::setfill('0') << std::setw(4) << std::hex << i + device.getOffset() << ":";
+		for (uint16_t j = i; j < i + 16; j++) {
+			if (j < device.getSize()) {
+				if (!(j % 2))
+					os << " ";
+				os << std::setfill('0') << std::setw(2) << std::hex << int(device.readByte(j + device.getOffset()));
 			}
-			os << std::endl << std::setfill('0') << std::setw(4) << std::hex << i << ":";
 		}
-		if (!(i % 2))
-			os << " ";
-		os << std::setfill('0') << std::setw(2) << std::hex << int(device.readByte(i + device.getOffset()));
+		os << "\t";
+		for (uint16_t j = i; j < i + 16; j++) {
+			if (j < device.getSize()) {
+				if (std::isprint(device.readByte(j + device.getOffset())))
+					os << device.readByte(j + device.getOffset());
+				else
+					os << ".";
+			}
+		}
 	}
 	return os << std::endl;;
 }
